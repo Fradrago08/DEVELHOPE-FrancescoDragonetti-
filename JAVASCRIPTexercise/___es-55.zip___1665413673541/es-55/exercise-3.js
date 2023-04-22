327b9b61bbe8514adf18c0024add18a992f5ca8f
Fra <kekkomaident@gmail.com>
2023-04-22 12:58:16 +0200

    esercizio13

---
+++ JAVASCRIPTexercise/___es-55.zip___1665413673541/es-55/exercise-3.js
@@ -0,0 +1,43 @@
+// function calculateAverageAge(persons) {
+//   // ...
+// }
+
+// const persons = [
+//   { name: 'Paul', age: 16 },
+//   { name: 'George', age: 17 },
+//   { name: 'Lucas', age: 21 },
+//   { name: 'Marco', age: 32 },
+//   { name: 'Peter', age: 18 },
+//   { name: 'Carl', age: 13 },
+//   { name: 'Simon', age: 24 },
+//   { name: 'Mark', age: 15 },
+//   { name: 'Sandra', age: 34 },
+//   { name: 'Alice', age: 28 }
+// ];
+
+// const average = calculateAverageAge(persons);
+// console.log(persons);
+// console.log(average);
+
+function calculateAverageAge(persons) {
+  const totalAge = persons.reduce((accumulator, currentValue) => accumulator + currentValue.age , 0 )
+  const avgAge = totalAge / persons.length;
+  return avgAge;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = Math.round(calculateAverageAge(persons));
+console.log(persons);
+console.log(average);
