32e8d64a2d5d9ddc06b42486223cfec4eae8ef25
Fra <kekkomaident@gmail.com>
2023-04-22 12:11:21 +0200

    esercizio4

---
+++ JAVASCRIPTexercise/___es-10.zip___1665409360234/es-10/exercise-7.js
@@ -0,0 +1,15 @@
+// function sumUntil(maxValue) {
+//   // ...
+// }
+
+// console.log(sumUntil(5));
+
+function sumUntil(maxValue) {
+  let sum = 0 ;
+  for ( let i = 0 ; i <= maxValue; i++){
+    sum = sum += i;
+  }
+  return sum;
+}
+
+console.log(sumUntil(5));
