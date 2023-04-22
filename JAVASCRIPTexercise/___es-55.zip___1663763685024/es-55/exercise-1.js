35841a00ebea6c2639debf972bdd6e78c01fb322
Fra <kekkomaident@gmail.com>
2023-04-22 13:24:08 +0200

    esercizio16

---
+++ JAVASCRIPTexercise/___es-55.zip___1663763685024/es-55/exercise-1.js
@@ -0,0 +1,14 @@
+// // Class definition
+
+// const developer = new Person('Mario', 'Rossi');
+// console.log(developer.firstName + " " + developer.lastName);
+
+
+class Person {
+    constructor(firstName,lastName){
+        this.firstName = firstName;
+        this.lastName= lastName;
+    }
+}
+const developer = new Person('Mario', 'Rossi');
+console.log(developer.firstName + " " + developer.lastName);
