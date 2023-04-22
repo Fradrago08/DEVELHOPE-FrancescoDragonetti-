f421aa44b201a9240ff1a16072a855fa8db060ea
Fra <kekkomaident@gmail.com>
2023-04-22 12:33:16 +0200

    esercizio9

---
+++ JAVASCRIPTexercise/___es-23.zip___1665409932103/es-23/exercise-1.js
@@ -0,0 +1,14 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+// Print values of person using Object.keys
+
+let keys = Object.keys(person);
+
+
+keys.forEach((el) => {
+  console.log(person[el]);
+})
