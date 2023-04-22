fbe4e960012a93be77c6956555e4919f40865611
Fra <kekkomaident@gmail.com>
2023-04-22 12:07:43 +0200

    esercizio3

---
+++ JAVASCRIPTexercise/___es-13.zip___1665409543938/es-13/exercise-3.js
@@ -0,0 +1,33 @@
+// const sum = function (a, b) {
+//   return a + b;
+// }
+
+// const subtract = function (a, b) {
+//   return a - b;
+// }
+
+// const multiply = function (a, b) {
+//   return a * b;
+// }
+
+// const divide = function (a, b) {
+//   return a / b;
+// }
+
+// const log = function (value) {
+//   console.log(value);
+// }
+
+
+const sum = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const log = (a,b,c) => 
+divide(subtract(multiply(sum(a,b), sum(c,a)), a), c);
+
+console.log(log(2,4,5));
