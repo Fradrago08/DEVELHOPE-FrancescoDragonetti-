ec0a186c0cb3950706e8834dfa50aea4234c3fa2
Fra <kekkomaident@gmail.com>
2023-04-22 13:28:01 +0200

    esercizio17

---
+++ JAVASCRIPTexercise/______es-56.zip___1663763760054.zip___1675957486717/es-56/exercise-2.js
@@ -0,0 +1,30 @@
+// class Person {
+//   constructor(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+// }
+
+// const developer = new Developer("Mario", "Rossi", "Front-end");
+// console.log(
+//   developer.firstName + " " + developer.lastName + " " + developer.role
+// );
+
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+class Developer extends Person{
+  constructor(firstName, lastName, role){
+    super(firstName,lastName);
+    this.role = role;
+  }
+}
+
+const developer = new Developer("Mario", "Rossi", "Front-end");
+console.log(
+  developer.firstName + " " + developer.lastName + " " + developer.role
+);
