19ac25ffbc695889cd58f4d73bcafe026c218120
Fra <kekkomaident@gmail.com>
2023-04-22 12:29:15 +0200

    esercizio8

---
+++ JAVASCRIPTexercise/___es-39.zip___1665399480338/es-39/exercise.js
@@ -0,0 +1,13 @@
+// function multiplyByTwo(value) {
+//   let number = 2;
+// }
+
+function multiplyByTwo(value) {
+  let number = 2;{
+    function inner(){
+      let multiply = value * number;
+      console.log(multiply);
+    }
+  }  inner()
+}
+console.log(multiplyByTwo(4));
